f802db0f0dfd7a7fff9bfd87e5fc8b82772e9316
Ayorinde Folorunso <ayorfolorunso@gmail.com>
2022-11-22 12:06:31 +0000

    Tuesday commit

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from pycaret.regression import *
 import pandas as pd
 import pickle
 import numpy as np
-
+from pycaret.time_series import TSForecastingExperiment
 app = Flask(__name__)
 
-model = load_model('my_best_model')
+
+# init environment
+
+exp = TSForecastingExperiment()
+model =exp.load_model('my_best_model')
 cols = ['Magna_6 Meteo Air Pressure (hpa)',
         'Magna_6 Meteo Dew Point Temperature (C)',
         'Magna_6 Water EC muS/cm',
@@ -34,16 +38,16 @@ def predict():
     int_features = [x for x in request.form.values()]
     final = np.array(int_features)
     data_unseen = pd.DataFrame([final], columns = cols)
-    prediction = predict_model(model, data=data_unseen, round = 0)
-    prediction = int(prediction.prediction_label[0])
+    prediction = exp.predict_model(model, X=data_unseen, round = 0)
+    prediction = int(prediction.y_pred[0])
     return render_template('home.html',pred='the moisture content is expected to be {}'.format(prediction))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)
     data_unseen = pd.DataFrame([data])
-    prediction = predict_model(model, data=data_unseen)
-    output = prediction.prediction_label[0]
+    prediction = exp.predict_model(model, data=data_unseen)
+    output = prediction.y_pred[0]
     return jsonify(output)
 
 if __name__ == '__main__':
