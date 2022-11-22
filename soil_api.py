af1926fddce238ae67ec9812aabe6f70a8c7180d
Ayorinde Folorunso <ayorfolorunso@gmail.com>
2022-11-22 23:21:17 +0000

    tuesday_modified

--- soil_api.py
+++ soil_api.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-from pycaret.time_series import load_model, predict_model, TSForecastingExperiment
+from pycaret.time_series import load_model, predict_model
 from fastapi import FastAPI
 import uvicorn
 from pydantic import create_model
 
 # Create the app
 app = FastAPI()
-exp = TSForecastingExperiment()
+
 # Load trained Pipeline
-model = exp.load_model("my_best_model")
+model = load_model("soil_api")
 
 # Create input/output pydantic models
-input_model = exp.create_model("soil_api_input", **{'Magna_6 Wind Direction': 226.5, 'Magna_6 Wind Speed (m/s)': 3.7, 'Magna_6 Meteo Ambient Temperature (C)': 8.4, 'Magna_6 Meteo Air Pressure (hpa)': 996.1, 'Magna_6 Meteo Relative Humidity': 94.3, 'Magna_6 Meteo Dew Point Temperature (C)': 7.5, 'Power Supply (V)': 13.089, 'Magna_6 Water EC muS/cm': 422.8, 'Magna_6 Water Level Above Sensor mm': 1754.6, 'segment1(EC)': 2.16, 'Magna_6 PH': 8.50717, 'Magna_6 ORP_mV': -539.455, 'Magna_6 pH_Sensor_Temperature_C': 8.85812, 'Wind Speed (x10)': 37.0, 'Wind Direction (x10)': 2265.0, 'Temperature (x10)': 84.0, 'Air Pressure (x10)': 9961.0, 'Relative Humidity (x10)': 943.0, 'Dew Point Temperature (x10)': 75.0, 'Magna_6 Precipitation_24hr_mm': 6.0, 'Magna_6 Cumulative Precipitation_mm': 44.0, 'Magna_6 CTD_Temperature_C': 8.5896, 'CTD_Pressure_BAR': 0.172068, 'CTD_Conductivity__mS': 0.422807, 'Metres above MSL': 0.0, 'TOW _MH2O': 0.0})
-output_model = exp.create_model("soil_api_output", moisture_prediction=92.72)
+input_model = create_model("soil_api_input", **{'Magna_6 Wind Direction': 328.2, 'Magna_6 Wind Speed (m/s)': 0.5, 'Magna_6 Meteo Ambient Temperature (C)': 7.1, 'Magna_6 Meteo Air Pressure (hpa)': 988.9, 'Magna_6 Meteo Relative Humidity': 98.3, 'Magna_6 Meteo Dew Point Temperature (C)': 6.9, 'Power Supply (V)': 13.115, 'Magna_6 Water EC muS/cm': 264.5, 'Magna_6 Water Level Above Sensor mm': 1685.9, 'segment1(EC)': 2.12, 'Magna_6 PH': 0.0, 'Magna_6 ORP_mV': 0.0, 'Magna_6 pH_Sensor_Temperature_C': 0.0, 'Magna_6 Precipitation_24hr_mm': 55.0, 'Magna_6 Cumulative Precipitation_mm': 308.0, 'Magna_6 CTD_Temperature_C': 8.848389, 'CTD_Pressure_BAR': 0.165328, 'CTD_Conductivity__mS': 0.264529, 'Metres above MSL': 206.26, 'TOW _MH2O': 0.77})
+output_model = create_model("soil_api_output", moisture_prediction=72.3)
 
 
 # Define predict function
 @app.post("/predict", response_model=output_model)
 def predict(data: input_model):
     data = pd.DataFrame([data.dict()])
-    predictions = exp.predict_model(model, data=data)
+    predictions = predict_model(model, data=data)
     return {"moisture_prediction": predictions["prediction_label"].iloc[0]}
 
 
