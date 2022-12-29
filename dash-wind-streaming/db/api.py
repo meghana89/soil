2e4dd064fbdb321cf71c27b0fb03e319c5365d08
Ayor Folorunso <Ayorfolorunso.gmail.com>
2022-12-29 13:52:22 +0000

    sqlite

--- dash-wind-streaming/db/api.py
+++ dash-wind-streaming/db/api.py
@@ -2,32 +2,26 @@ import pathlib
 import sqlite3
 import pandas as pd
 
-
-
-
-
 def get_wind_data():
     """
     Query wind data rows between two ranges
-
     :params start: start row id
     :params end: end row id
     :returns: pandas dataframe object
     """
     data = pd.read_csv("../soil.csv")
     data.set_index(['date'], inplace = True)
-    df= data["segment1(10-30cm)"]
+    df= data.copy()
     return df
 
 
 def get_wind_data_by_id(i):
     """
     Query a row from the Wind Table
-
     :params id: a row id
     :returns: pandas dataframe object
     """
-    df = pd.read_csv("../soil.csv")
-    df.set_index(['date'], inplace = True)
-
+    data = pd.read_csv("../soil.csv")
+    data.set_index(['date'], inplace = True)
+    df= data.copy()
     return df
